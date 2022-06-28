358ab39f8e18052ca1ec4463a50a7c6cf2a1913d
조준희 <iopp3423@gmail.com>
2022-06-28 12:09:07 +0900

    [UPDATE] 성별 체크박스 추가

--- lib/src/main/main.dart
+++ lib/src/main/main.dart
@@ -3,167 +3,209 @@ import 'dart:js';
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
+  //const MyApp({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(home: joinPage());
   }
 }
+  class joinPage extends StatefulWidget {
+    // 회원가입 페이지
+    @override
+    _joinPage createState() => _joinPage();
+  }
+  //joinPage({Key? key}) : super(key: key); // 원래 const jounPage였음
+  class _joinPage extends State<joinPage> {
+    bool valuefirst = false;
+    bool valuesecond = false;
 
-class joinPage extends StatelessWidget {
-  // 회원가입 페이지
-  const joinPage({Key? key}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text(
-          '회원정보 입력 및 동의',
-          style: TextStyle(
-              color: Colors.black, // 글자 색상 검정색
-              fontSize: 22.0, // 폰트 사이즈
-              fontWeight: FontWeight.bold // 폰트 굵기
-              ),
-        ),
-        leading: IconButton(
-          // 리딩 부분 (뒤로가기)
-          onPressed: () {
-            // 버튼
-            Navigator.pop(context); //뒤로가기
-          },
-          color: Colors.black, // 버튼 색상
-          icon: Icon(Icons.arrow_back), // 뒤로가기 이미지 아이콘
-        ),
-
-        centerTitle: true,
-        // 글자 중간으로 위치 지정
-        elevation: 0.0,
-        // 붕 떠 있는 효과를 줌
-        backgroundColor: Colors.white, // 배경색상 흰색
-      ),
-      body: Stack(
-        children: [
-          Positioned(
-            child: Container(
-              // 아이디 비밀번호 레이아웃 박스
-              padding: EdgeInsets.all(10.0),
-              decoration: BoxDecoration(
-                border: Border.all(color: Colors.white), // 테두리
-                borderRadius: BorderRadius.circular(5), //모서리 둥글게
-              ),
-
-              child: Form(
-                child: Theme(
-                  // 왜 스타일 적용 안되냐
-                  data: ThemeData(
-                      primaryColor: Colors.teal,
-                      inputDecorationTheme: InputDecorationTheme(
-                          labelStyle: TextStyle(
-                        fontSize: 15,
-                      ))),
-
-                  child: Column(
-                    children: [
-                      Flexible(
-                        child: TextFormField(
-                          decoration: InputDecoration(
-                            labelText: '아이디',
-                            filled: true,
-                            fillColor: Colors.white,
-                          ),
-                          keyboardType: TextInputType.text,
-                        ),
-                      ),
-                      Flexible(
-                        child: TextFormField(
-                          decoration: InputDecoration(
-                            labelText: '비밀번호',
-                            filled: true,
-                            fillColor: Colors.white,
-                          ),
-                          keyboardType: TextInputType.text,
-                          obscureText: true, // 비밀번호 암호처리
-                        ),
-                      ),
-                      Flexible(
-                        child: TextFormField(
-                          decoration: InputDecoration(
-                            labelText: '비밀번호확인',
-                            filled: true,
-                            fillColor: Colors.white,
-                          ),
-                          keyboardType: TextInputType.text,
-                          obscureText: true, // 비밀번호 암호처리
-                        ),
-                      ),
-                      Flexible(
-                        child: TextFormField(
-                          decoration: InputDecoration(
-                            labelText: '이메일',
-                            filled: true,
-                            fillColor: Colors.white,
-                          ),
-                          keyboardType: TextInputType.emailAddress,
-                        ),
-                      ),
-                      SizedBox(height: 30),
-                      Flexible(
-                        child: TextFormField(
-                          decoration: InputDecoration(
-                            labelText: '이름',
-                            filled: true,
-                            fillColor: Colors.white,
-                          ),
-                          keyboardType: TextInputType.name,
-                        ),
-                      ),
-                    ],
-                  ),
-                ), //아이디 비밀번호 레이아웃
-              ),
+    @override
+    Widget build(BuildContext context) {
+      return Scaffold(
+        appBar: AppBar(
+          title: const Text(
+            '회원정보 입력 및 동의',
+            style: TextStyle(
+                color: Colors.black, // 글자 색상 검정색
+                fontSize: 22.0, // 폰트 사이즈
+                fontWeight: FontWeight.bold // 폰트 굵기
             ),
           ),
-        ],
-      ),
-      bottomNavigationBar: BottomAppBar(
-        // 밑에 하단바(회원가입 버튼)
-        child: Container(
-            height: 50,
-            child: ElevatedButton(
-              onPressed: () {
-                showDialog(
-                  context: context,
-                  barrierDismissible: false, // 바깥 영역 터치시 닫을지 여부
-                  builder: (BuildContext context) {
-                    // return object of type Dialog
-                    return AlertDialog(
-                      title: Text("안내메시지"),
-                      content: Text("회원가입이 완료되었습니다!"),
-                      actions: <Widget>[
-                        FlatButton(
-                          child: Text("닫기"),
-                          onPressed: () {
-                            Navigator.pop(context);
-                          },
+          leading: IconButton(
+            // 리딩 부분 (뒤로가기)
+            onPressed: () {
+              // 버튼
+              Navigator.pop(context); //뒤로가기
+            },
+            color: Colors.black, // 버튼 색상
+            icon: Icon(Icons.arrow_back), // 뒤로가기 이미지 아이콘
+          ),
+
+          centerTitle: true,
+          // 글자 중간으로 위치 지정
+          elevation: 0.0,
+          // 붕 떠 있는 효과를 줌
+          backgroundColor: Colors.white, // 배경색상 흰색
+        ),
+        body: Stack(
+          children: [
+            Positioned(
+              child: Container(
+                // 아이디 비밀번호 레이아웃 박스
+                padding: EdgeInsets.all(10.0),
+                decoration: BoxDecoration(
+                  border: Border.all(color: Colors.white), // 테두리
+                  borderRadius: BorderRadius.circular(5), //모서리 둥글게
+                ),
+
+                child: Form(
+                  child: Theme(
+                    // 왜 스타일 적용 안되냐
+                    data: ThemeData(
+                        primaryColor: Colors.teal,
+                        inputDecorationTheme: InputDecorationTheme(
+                            labelStyle: TextStyle(
+                              fontSize: 15,
+                            ))),
+
+                    child: Column(
+                      children: [
+                        Flexible(
+                          child: TextFormField(
+                            decoration: InputDecoration(
+                              labelText: '아이디',
+                              filled: true,
+                              fillColor: Colors.white,
+                            ),
+                            keyboardType: TextInputType.text,
+                          ),
+                        ),
+                        Flexible(
+                          child: TextFormField(
+                            decoration: InputDecoration(
+                              labelText: '비밀번호',
+                              filled: true,
+                              fillColor: Colors.white,
+                            ),
+                            keyboardType: TextInputType.text,
+                            obscureText: true, // 비밀번호 암호처리
+                          ),
+                        ),
+                        Flexible(
+                          child: TextFormField(
+                            decoration: InputDecoration(
+                              labelText: '비밀번호확인',
+                              filled: true,
+                              fillColor: Colors.white,
+                            ),
+                            keyboardType: TextInputType.text,
+                            obscureText: true, // 비밀번호 암호처리
+                          ),
+                        ),
+                        Flexible(
+                          child: TextFormField(
+                            decoration: InputDecoration(
+                              labelText: '이메일',
+                              filled: true,
+                              fillColor: Colors.white,
+                            ),
+                            keyboardType: TextInputType.emailAddress,
+                          ),
+                        ),
+                        Flexible(
+                          child: TextFormField(
+                            decoration: InputDecoration(
+                              labelText: '이름',
+                              filled: true,
+                              fillColor: Colors.white,
+                            ),
+                            keyboardType: TextInputType.name,
+                          ),
+                        ),
+                        SizedBox(height: 30),
+                        Flexible(
+                          child: TextFormField(
+                            decoration: InputDecoration(
+                              labelText: '생년월일',
+                              filled: true,
+                              fillColor: Colors.white,
+                            ),
+                            keyboardType: TextInputType.name,
+                          ),
+                        ),
+                        Row(
+                          children: <Widget>[
+                            SizedBox(width: 10,),
+                            Text('성별', style: TextStyle(fontSize: 17.0),),
+                            Checkbox(
+                              checkColor: Colors.greenAccent,
+                              activeColor: Colors.red,
+                              value: this.valuefirst,
+                              onChanged: (value) {
+                                setState(() {
+                                  this.valuefirst = value!;
+                                });
+                              },
+                            ),
+                            Checkbox(
+                              value: this.valuesecond,
+                              onChanged: (value) {
+                                setState(() {
+                                  this.valuesecond = value!;
+                                });
+                              },
+                            ),
+                          ],
                         ),
                       ],
-                    );
-                  },
-                );
-              },
-              child: Text("회원가입"),
-              style: ElevatedButton.styleFrom(
-              primary: Colors.grey, // 버튼색상
-               onPrimary: Colors.black, // 글자색상
+                    ),
+                  ), //아이디 비밀번호 레이아웃
+                ),
               ),
-            )),
-      ),
-    );
+            ),
+          ],
+        ),
+
+        bottomNavigationBar: BottomAppBar(
+          // 밑에 하단바(회원가입 버튼)
+          child: Container(
+              height: 50,
+              child: ElevatedButton(
+                onPressed: () {
+                  showDialog(
+                    context: context,
+                    barrierDismissible: false, // 바깥 영역 터치시 닫을지 여부
+                    builder: (BuildContext context) {
+                      // return object of type Dialog
+                      return AlertDialog(
+                        title: Text("안내메시지"),
+                        content: Text("회원가입이 완료되었습니다!"),
+                        actions: <Widget>[
+                          FlatButton(
+                            child: Text("닫기"),
+                            onPressed: () {
+                              Navigator.pop(context);
+                            },
+                          ),
+                        ],
+                      );
+                    },
+                  );
+                },
+                child: Text("회원가입"),
+                style: ElevatedButton.styleFrom(
+                  primary: Colors.grey, // 버튼색상
+                  onPrimary: Colors.black, // 글자색상
+                ),
+              )),
+        ),
+      );
+    }
   }
-}
+
+
