c9de53b56f72a608b51aef62660e0a4f94fbf908
littleemm <lilysolar@naver.com>
2022-06-29 14:17:32 +0900

    [ADD] 산 정보 api 연결 파일 추가

---
+++ lib/src/api/forestInformationApi.dart
@@ -0,0 +1,21 @@
+import 'package:dio/dio.dart';
+
+class ForestInformationApi {
+  late var dio;
+
+  ForestInformationApi() {
+    dio = Dio(
+      BaseOptions(
+          baseUrl: "https://openapi.forest.go.kr",
+          queryParameters: {
+            'serviceKey':
+            'WRW8U5mI32TQ7scwZY7OauPpIOPTEz2o5RcdqnpvaCWeow%2FzFQ3xRp7uciHXufIHRPIDA%2Be9%2FJBjfYCyfy4bTg%3D%3D'
+          },
+      ),
+    );
+  }
+
+  fetchMountainInformation()async{
+    var response = await dio.get('/openapi/service/trailInfoService/getforeststoryservice');
+  }
+}
