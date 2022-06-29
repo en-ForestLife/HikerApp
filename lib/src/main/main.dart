bc392892e0ddda53f8da23a4eb8cc248f1dfef64
littleemm <lilysolar@naver.com>
2022-06-30 02:27:55 +0900

    [FIX] controller 못찾는 오류 -> main.dart 코드 추가해서 오류 수정

--- lib/src/main/main.dart
+++ lib/src/main/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:hiker/src/controller/forestInformationController.dart';
 import './mainPage.dart';
 
 void main() {
@@ -11,6 +12,9 @@ class HikerApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return GetMaterialApp(
+      initialBinding: BindingsBuilder(() {
+        Get.put(ForestInformationController());
+      }),
       home:MainPage(),
     );
   }
