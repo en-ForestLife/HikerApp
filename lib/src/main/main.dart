f7faec3a59069cb292ad7b237565f8b522053c81
조준희 <iopp3423@gmail.com>
2022-06-30 12:22:15 +0900

    [ADD] 회원가입시 아이디, 이메일 중복체크 코드 추가

--- lib/src/main/main.dart
+++ lib/src/main/main.dart
@@ -1,11 +1,21 @@
 import 'dart:js';
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import '../utils/checkInformation.dart';
-//import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 
 void main() async{
   WidgetsFlutterBinding.ensureInitialized();
- // Firebase.initializeApp();
+  await Firebase.initializeApp(
+    options: FirebaseOptions(
+    apiKey: 'AIzaSyCixnmrkfi9PHOKja6DKeIsj_K3yExnXlk',
+    appId: '1:974972266799:android:f1f8fbac8571a5438959da',
+    messagingSenderId: '974972266799',
+    projectId: 'enforestlife-d26ef',
+    storageBucket: 'enforestlife-d26ef.appspot.com',
+    ),
+  );
   runApp(MyApp());
 }
 
@@ -26,6 +36,9 @@ class joinPage extends StatefulWidget {
 
 //joinPage({Key? key}) : super(key: key); // 원래 const jounPage였음
 class _joinPage extends State<joinPage> {
+  final _authentication = FirebaseAuth.instance;
+  FirebaseFirestore fireStore=FirebaseFirestore.instance;
+
   bool valuefirst = false;
   bool valuesecond = false;
 
@@ -34,6 +47,9 @@ class _joinPage extends State<joinPage> {
   String email = '';
   String name = '';
   String birth = '';
+  String sex = '';
+
+  var message = '';
 
   FocusNode _emailFocus = new FocusNode();
   FocusNode _passwordFocus = new FocusNode();
@@ -95,11 +111,12 @@ class _joinPage extends State<joinPage> {
                   child: Column(
                     children: [
                       idInput(), // 아이디
+                      Email(), // 이메일 입력
                       passwordInput(), // 비밀번호
                       passwordCheck(), // 비밀번호 체크
-                      Email(), // 이메일 입력
                       nameInput(), // 이름 입력
-                      birthInput(), // 생일 입
+                      birthInput(), // 생일 입력
+
                       Container(
                         color: Colors.white,
                         height: 50,
@@ -136,6 +153,7 @@ class _joinPage extends State<joinPage> {
                                 if (!this.valuesecond) // 남녀 하나만 선택
                                   setState(() {
                                     this.valuefirst = value!;
+                                      sex = this.valuefirst.toString();
                                   });
                               },
                             ),
@@ -149,12 +167,14 @@ class _joinPage extends State<joinPage> {
                                 if (!this.valuefirst) // 남녀 하나만 선택히
                                   setState(() {
                                     this.valuesecond = value!;
+                                      sex = this.valuefirst.toString();
                                   });
                               },
                             ),
                           ],
                         ),
                       ),
+
                     ],
                   ),
                 ), //아이디 비밀번호 레이아웃
@@ -171,7 +191,29 @@ class _joinPage extends State<joinPage> {
             height: 50,
             child: ElevatedButton(
               onPressed: () {
-                checkValidation();
+
+                bool check = checkIdAndEmail();
+                if(checkValidation() && !check){ // 회원정보 다 채우고 아이디 또는 비밀번호가 중복이 없을 때
+                    showDialog(
+                      context: context,
+                      barrierDismissible: false, // 바깥 영역 터치시 닫을지 여부
+                      builder: (BuildContext context) {
+                        // return object of type Dialog
+                        return AlertDialog(
+                          title: Text("안내메시지"),
+                          content: Text(message),
+                          actions: <Widget>[
+                            FlatButton(
+                              child: Text("닫기"),
+                              onPressed: () {
+                                Navigator.pop(context);
+                              },
+                            ),
+                          ],
+                        );
+                      },
+                    );
+                  /*
                   showDialog(
                     context: context,
                     barrierDismissible: false, // 바깥 영역 터치시 닫을지 여부
@@ -184,7 +226,6 @@ class _joinPage extends State<joinPage> {
                           FlatButton(
                             child: Text("닫기"),
                             onPressed: () {
-                              print(id);
                               Navigator.pop(context);
                             },
                           ),
@@ -192,6 +233,29 @@ class _joinPage extends State<joinPage> {
                       );
                     },
                   );
+*/
+                  fireStore.collection('User').where("id", isEqualTo: email).get().then( // 이건 함 찾아봐야 할듯
+                        (res) => print('SUCCESS'),
+                    onError: (e) => print("Error completing: $e"),
+                  );
+                  _getSubCnt(); // 도큐먼트 수 알려줌
+                }
+
+                /*
+                final newUser = _authentication.createUserWithEmailAndPassword(
+                    email: email,
+                    password: password
+                );*/
+/*
+                fireStore.collection('User').doc().set({
+                  "id" : id,
+                  "email" : email,
+                  "password" : password,
+                  "name" : name,
+                  "birth" : birth,
+                  "sex" : sex,
+                });
+                */
               },
               child: Text("회원가입"),
               style: ElevatedButton.styleFrom(
@@ -212,7 +276,9 @@ class _joinPage extends State<joinPage> {
           keyboardType: TextInputType.text,
           decoration: _textFormDecoration('영문 + 숫자 조합 4~12자', '아이디'),
           autovalidateMode: AutovalidateMode.onUserInteraction,
-          onChanged: (dynamic val) {},
+          onChanged: (dynamic val) {
+            id = val;
+          },
           validator: (value) =>
               CheckValidate().validateId(value.toString()),
           onSaved : (value){
@@ -234,7 +300,9 @@ class _joinPage extends State<joinPage> {
           decoration:
               _textFormDecoration('영문 대.소문자 + 숫자 + 특수문자 조합 8~15자', '비밀번호'),
           autovalidateMode: AutovalidateMode.onUserInteraction,
-          onChanged: (dynamic val) {},
+          onChanged: (dynamic val) {
+            password = val;
+          },
           validator: (value) =>
               CheckValidate().validatePassword(value.toString()),
           onSaved : (value){
@@ -283,7 +351,9 @@ class _joinPage extends State<joinPage> {
           keyboardType: TextInputType.emailAddress,
           decoration: _textFormDecoration('이메일 형식에 맞게 입력', '이메일'),
           autovalidateMode: AutovalidateMode.onUserInteraction,
-          onChanged: (dynamic val) {},
+          onChanged: (dynamic val) {
+            email = val;
+          },
           validator: (value) =>
               CheckValidate().validateEmail(value.toString()),
           onSaved : (value){
@@ -304,7 +374,9 @@ class _joinPage extends State<joinPage> {
           decoration:
           _textFormDecoration('실명입력', '이름'),
           autovalidateMode: AutovalidateMode.onUserInteraction,
-          onChanged: (dynamic val) {},
+          onChanged: (dynamic val) {
+            name = val;
+          },
           validator: (value) =>
               CheckValidate().validateName(value.toString()),
           onSaved : (value){
@@ -326,7 +398,9 @@ class _joinPage extends State<joinPage> {
           decoration:
           _textFormDecoration('ex) 1999-08-20', '생년월일'),
           autovalidateMode: AutovalidateMode.onUserInteraction,
-          onChanged: (dynamic val) {},
+          onChanged: (dynamic val) {
+            birth = val;
+          },
           validator: (value) =>
               CheckValidate().validateBirth(value.toString()),
           onSaved : (value){
@@ -367,12 +441,43 @@ class _joinPage extends State<joinPage> {
       fillColor: Colors.white,
     );
   }
-
-  void checkValidation(){
+  void _getSubCnt() { // 컬렉션 안의 도큐먼트 갯수 가져오기
+    FirebaseFirestore.instance
+        .collection('User')
+        .get()
+        .then((snapShot) {
+      var qTotal = snapShot.docs.length;
+      print(qTotal);
+    });
+  }
+  bool checkValidation(){
     final isValid = formKey.currentState!.validate();
     if(isValid){
       formKey.currentState!.save();
     }
+    return isValid;
+  }
+
+  bool checkIdAndEmail(){
+    bool check = false;
+    fireStore // 아이디, 이메일 중복체크인데 for문으로 찾아서 where로 찾을 수 있나 찾아봐야함
+        .collection('User')
+        .snapshots()
+        .listen((data){
+      data.docs.forEach((element) {
+        if(element['id'] == id){
+          message = '이미 존재하는 아이디입니다.';
+          check = true;
+        }
+        else if(element['email'] == email){
+          message = '이미 존재하는 이메일입니다.';
+          check = true;
+        }
+        else message = '회원가입이 완료되었습니다.';
+      });
+    });
+
+    return check;
   }
 
 }
