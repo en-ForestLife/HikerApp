eb226c06af7d35c08d9bf2d9ab99da5ab5c5f967
DaeWon <qkreodnjs97@naver.com>
2022-07-03 00:13:17 +0900

    [FIX] 충돌처리

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -12,50 +12,5 @@ import 'package:hiker/src/utility/xmlUtils.dart';
 import 'package:xml/xml.dart';
 
 void main() {
-  testWidgets('Counter increments smoke test', (WidgetTester tester) async {
-    // Build our app and trigger a frame.
-    await tester.pumpWidget(MyApp());
-  test('산 정보', () {
-    final document = XmlDocument.parse(bookshelfXml);
-    final items = document.findAllElements('item');
-    var forestInformation = <ForestInformationModel>[];
-    items.forEach((node) {
-      forestInformation.add(ForestInformationModel.fromXml(node));
-    });
-  });
-}
 
-class ForestInformationModel {
-  String? mntnnm;
-  String? mntnattchimageseq;
-  String? mntninfopoflc;
-  String? mntnsbttlinfo;
-  String? mntninfohght;
-
-  ForestInformationModel({
-    this.mntnnm,
-    this.mntnattchimageseq,
-    this.mntninfopoflc,
-    this.mntnsbttlinfo,
-    this.mntninfohght,
-  });
-
-  factory ForestInformationModel.fromXml(XmlElement xml) {
-    return ForestInformationModel(
-      mntnnm : XmlUtils.searchResult(xml, 'mntnnm'),
-      mntnattchimageseq : XmlUtils.searchResult(xml, 'mntnattchimageseq'),
-      mntninfopoflc : XmlUtils.searchResult(xml, 'mntninfopoflc'),
-      mntnsbttlinfo : XmlUtils.searchResult(xml, 'mntnsbttlinfo'),
-      mntninfohght : XmlUtils.searchResult(xml, 'mntninfohght'),
-    );
-  }
-  }
-
-
-class XmlUtils {
-  static String searchResult(XmlElement xml, String key) {
-    return xml.findAllElements(key).map((e) => e.text).isEmpty
-        ? ""
-        : xml.findAllElements(key).map((e) => e.text).first;
-  }
 }
