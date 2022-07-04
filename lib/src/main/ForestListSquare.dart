cb9f6a65d7bdd4ac68da3687c5929e3c3654711a
littleemm <lilysolar@naver.com>
2022-07-04 18:01:00 +0900

    [REMOVE] api 이미지 url 설정하는 코드 따로 함수로 빼서 코드 옮김

--- lib/src/main/ForestListSquare.dart
+++ lib/src/main/ForestListSquare.dart
@@ -1,12 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:get/get_state_manager/src/rx_flutter/rx_obx_widget.dart';
 import 'package:get/get_state_manager/src/simple/get_view.dart';
-
+import 'package:intl/intl.dart';
 import '../controller/forestInformationController.dart';
 import '../model/forestInformation.dart';
 
 class ForestListSquare extends GetView<ForestInformationController> {
   int index;
+
   ForestListSquare(this.index);
 
   @override
@@ -18,38 +19,53 @@ class ForestListSquare extends GetView<ForestInformationController> {
         height: 510,
         child: Obx(() {
           var information = controller.forestInformation.value;
-          String imageUrl = information[index].mntnattchimageseq.toString();
+          String imageUrl = setUrl();
 
-          if(!imageUrl.contains("FILE")) {
-            imageUrl = 'https://ifh.cc/g/FapjP1.png';
-          }
-
-         return Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget> [
-              ClipRRect(
-                borderRadius: BorderRadius.circular(15.0),
-                child:
-                Image.network(imageUrl,
-                    width:450,
-                    height:400,
+          return Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                ClipRRect(
+                  borderRadius: BorderRadius.circular(15.0),
+                  child:
+                  Image.network(imageUrl,
+                    width: 450,
+                    height: 400,
                     fit: BoxFit.fill,
+                  ),
                 ),
-              ),
-              Column(
-                  crossAxisAlignment: CrossAxisAlignment.center,
-                children:<Widget> [
-                  Text('\n', style:TextStyle(fontSize:5)),
-                  Text(information[index].mntnnm ?? '', style:TextStyle(fontSize: 14, fontWeight: FontWeight.bold, letterSpacing: 2.0), textAlign: TextAlign.left,),
-                  Text(information[index].mntnsbttlinfo ?? '', style: TextStyle(fontSize: 14,)),
-                  Text(information[index].mntninfopoflc ?? '', style: TextStyle(fontSize: 14,)),
-                  Text(information[index].mntninfohght ?? '', style: TextStyle(fontSize: 14,)),
+                Column(
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: <Widget>[
+                      Text('\n', style: TextStyle(fontSize: 5)),
+                      Text(information[index].mntnnm ?? '', style: TextStyle(
+                          fontSize: 14,
+                          fontWeight: FontWeight.bold,
+                          letterSpacing: 2.0), textAlign: TextAlign.left,),
+                      Text(information[index].mntnsbttlinfo ?? '',
+                          style: TextStyle(fontSize: 14,)),
+                      Text(information[index].mntninfopoflc ?? '',
+                          style: TextStyle(fontSize: 14,)),
+                      Text(information[index].mntninfohght ?? '',
+                          style: TextStyle(fontSize: 14,)),
+                    ]
+                )
               ]
-              )
-            ]
-        );
-  }),
-        ),
+          );
+        }),
+      ),
     );
   }
+
+  String setUrl() { // api ui 이미지 불러와지는지 판단한 후 이미지 내보내기
+    var 기nformation = controller.forestInformation.value;
+    String imageUrl = information[index].mntnattchimageseq.toString();
+
+    if (imageUrl.contains("FILE")) {
+      return imageUrl;
+    }
+
+    return 'https://ifh.cc/g/FapjP1.png';
+  }
+
+
 }
