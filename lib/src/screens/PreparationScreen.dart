3fa246fc7ceba72d16271e78c7395a53ac2bf471
DaeWon <qkreodnjs97@naver.com>
2022-07-06 19:22:24 +0900

    [ADD] 페이지간 연결 추가

--- lib/src/screens/PreparationScreen.dart
+++ lib/src/screens/PreparationScreen.dart
@@ -47,7 +47,7 @@ class _PreparationScreenState extends State<PreparationScreen> {
           showDetail(name, detailInfo);
         },
         child: SizedBox(
-          height: 150,
+          height: 120,
           child: Row(
             //mainAxisAlignment: MainAxisAlignment.center,
             children: [
