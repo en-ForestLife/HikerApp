e571c31be176aa9fc327350a92cd9d3c08ecb9bc
littleemm <lilysolar@naver.com>
2022-07-06 23:20:24 +0900

    [ADD] TIP-등산TIP 페이지 추가

--- lib/src/main/ForestListSquare.dart
+++ lib/src/main/ForestListSquare.dart
@@ -1,3 +1,9 @@
+import 'dart:html';
+import 'dart:js';
+
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get_rx/src/rx_types/rx_types.dart';
 import 'package:get/get_state_manager/src/rx_flutter/rx_obx_widget.dart';
@@ -18,40 +24,42 @@ class ForestListSquare extends GetView<ForestInformationController> {
         height: 510,
         child: Obx(() {
           var information = controller.forestInformation.value.obs;
-          print(information);
-          print('산산');
           String imageUrl = getUrl(information);
 
-          return Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: <Widget>[
-
-                Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: <Widget>[
-                      ClipRRect(
-                        borderRadius: BorderRadius.circular(15.0),
-                        child:
-                        Image.network(imageUrl,
-                          width: 450,
-                          height: 400,
-                          fit: BoxFit.fill,
+          return  Container(
+            child : Column(
+                children: <Widget>[
+                  Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: <Widget>[
+                        ClipRRect(
+                          borderRadius: BorderRadius.circular(15.0),
+                          child:
+                          Image.network(imageUrl,
+                            width: double.infinity,
+                            height: 400,
+                            fit: BoxFit.fill,
+                          ),
                         ),
-                      ),
-                      Text('\n', style: TextStyle(fontSize: 5)),
-                      Text(information[index].mntnnm ?? '', style: TextStyle(
-                          fontSize: 14,
-                          fontWeight: FontWeight.bold,
-                          letterSpacing: 2.0), textAlign: TextAlign.left,),
-                      Text(addSubtitle(information),
-                          style: TextStyle(fontSize: 14,)),
-                      Text(information[index].mntninfopoflc ?? '',
-                          style: TextStyle(fontSize: 14,)),
-                      Text(getHeightFormat(information),
-                          style: TextStyle(fontSize: 14,)),
-                    ]
-                )
-              ]
+                        Text('\n', style: TextStyle(fontSize: 5)),
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children:[
+                            Text(information[index].mntnnm ?? '', style: TextStyle(
+                                fontSize: 14,
+                                fontWeight: FontWeight.bold,
+                                letterSpacing: 2.0),),],
+                        ),
+                        Text(addSubtitle(information),
+                            style: TextStyle(fontSize: 14,)),
+                        Text(information[index].mntninfopoflc ?? '',
+                            style: TextStyle(fontSize: 14,)),
+                        Text(getHeightFormat(information),
+                            style: TextStyle(fontSize: 14,)),
+                      ]
+                  )
+                ]
+            ),
           );
         }),
       ),
@@ -80,3 +88,5 @@ class ForestListSquare extends GetView<ForestInformationController> {
     return subTitle;
   }
 }
+
+
